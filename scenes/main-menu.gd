b8cb8a36c38c307372ffcdbdb68a53070f1022fa
Brendan <brendanweirtz@gmail.com>
2021-12-28 21:08:44 -0800

    added main menu

---
+++ scenes/main-menu.gd
@@ -0,0 +1,9 @@
+extends MarginContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_MenuButton_pressed():
+	get_tree().change_scene("res://scenes/ball_test_playground.tscn")
