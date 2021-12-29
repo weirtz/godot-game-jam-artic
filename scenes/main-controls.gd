5872b832e5ec07cd610a58c0cc471a6e258ddf8e
Brendan <brendanweirtz@gmail.com>
2021-12-28 19:38:13 -0500

    Game Jam

--- scenes/main-controls.gd
+++ scenes/main-controls.gd
@@ -9,6 +9,12 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	print("Remove GUI from player node")
 	print("Add input restart.")
+	
+	#set game window
+	OS.window_size = Vector2(1280,720)
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 
 	
 func _process(delta):
